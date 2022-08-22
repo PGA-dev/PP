ee1c6de33ebaaf07cd46186cddb6c101ae8ee016
PGA <paulgadam@gmail.com>
2022-08-22 03:34:48 -0600

    database add with grocery subapp

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -11,3 +11,18 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET default_tablespace = '';
 SET default_with_oids = false;
+
+CREATE TABLE IF NOT EXISTS grocery(
+    id serial NOT NULL,
+    groc_item_title varchar NOT NULL,
+    groc_item_price decimal NOT NULL,
+    groc_item_cat varchar NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS budget(
+    id serial NOT NULL,
+    total_budget decimal NOT NULL,
+    item_budget decimal NOT NULL,
+    CONSTRAINT id PRIMARY KEY (id)
+);
