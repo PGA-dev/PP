e317d1531d839c8b5e2a9d2ce4e331c6ad9d2f75
PGA <paulgadam@gmail.com>
2022-08-25 19:34:38 -0600

    update to Django_init add tables

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -1,17 +1,6 @@
 DROP DATABASE IF EXISTS grocery_budget_db;
 CREATE DATABASE grocery_budget_db;
 
-\c grocery_budget_db
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET default_tablespace = '';
-SET default_with_oids = false;
-
 CREATE TABLE IF NOT EXISTS grocery(
     id serial NOT NULL,
     groc_item_title varchar NOT NULL,
@@ -26,3 +15,15 @@ CREATE TABLE IF NOT EXISTS budget(
     item_budget decimal NOT NULL,
     CONSTRAINT id PRIMARY KEY (id)
 );
+
+\c grocery_budget_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
+
