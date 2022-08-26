e35e8791878483e20b0b629b3fcb68eccdb0e161
PGA <paulgadam@gmail.com>
2022-08-25 19:40:33 -0600

    comment out db script tables

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -1,20 +1,20 @@
 DROP DATABASE IF EXISTS grocery_budget_db;
 CREATE DATABASE grocery_budget_db;
 
-CREATE TABLE IF NOT EXISTS grocery(
-    id serial NOT NULL,
-    groc_item_title varchar NOT NULL,
-    groc_item_price decimal NOT NULL,
-    groc_item_cat varchar NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS grocery(
+--     id serial NOT NULL,
+--     groc_item_title varchar NOT NULL,
+--     groc_item_price decimal NOT NULL,
+--     groc_item_cat varchar NOT NULL,
+--     CONSTRAINT id PRIMARY KEY (id)
+-- );
 
-CREATE TABLE IF NOT EXISTS budget(
-    id serial NOT NULL,
-    total_budget decimal NOT NULL,
-    item_budget decimal NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
-);
+-- CREATE TABLE IF NOT EXISTS budget(
+--     id serial NOT NULL,
+--     total_budget decimal NOT NULL,
+--     item_budget decimal NOT NULL,
+--     CONSTRAINT id PRIMARY KEY (id)
+-- );
 
 \c grocery_budget_db
 
